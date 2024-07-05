facf88e3e7b1b604193876c600a9cbc3dbae6fa4
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-06 06:34:51 +0700

    Update default.js

--- src/handler/command/default.js
+++ src/handler/command/default.js
@@ -1,4 +1,3 @@
-let debug = require('debug')("ez:commands")
 module.exports = async (msg, real) => {
     const client = real.client;
     const db = real.db;
@@ -8,14 +7,14 @@ module.exports = async (msg, real) => {
     let name = Array.from(cmd.default).filter(z => body.startsWith(z[0])).map(z => z[0]).toString();
     let cmds = cmd.default.get(name);
 
-    let withPref = cmds?.withPrefix == undefined || cmds?.withPrefix == true ? msg.content?.toLowerCase().startsWith(client?.prefix) : !lower.startsWith(client.prefix) ? true : false
-    debug("Message event prams: ", withPref)
-    if (withPref && cmds?.code && !msg.author?.bot) {
-        debug("Code is a string")
-        require("../function.js")(cmds.code, name, db, msg, client, real)
-    } else if (withPref && cmds?.execute && !msg.author?.bot && typeof cmds.execute === 'function') {
-        debug("code is a function")
-        require("../function.js")(cmds.execute, name, db, msg, client, real)
-    }
+    let withPref = cmds?.withPrefix == undefined || cmds?.withPrefix == true ? msg.content?.toLowerCase().startsWith(client?.prefix) : !lower.startsWith(client.prefix) ? true : false;
+    console.log("Message event params: ", withPref);
 
-}
+    if (withPref && cmds?.code && !msg.author?.bot) {
+        console.log("Code is a string");
+        require("../function.js")(cmds.code, name, db, msg, client, real);
+    } else if (withPref && cmds?.execute && !msg.author?.bot && typeof cmds.execute === 'function') {
+        console.log("Code is a function");
+        require("../function.js")(cmds.execute, name, db, msg, client, real);
+    }
+};
