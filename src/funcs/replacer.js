202398ee18576cf0b63d2816abe425564887ab7c
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-06 06:28:41 +0700

    Update replacer.js

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -1,35 +1,33 @@
 module.exports = async (data, name, db, message, client, error, real) => {
-    /* const parse = fs.readdirSync(__dirname + "/functions")*/
-
     function err(d, message) {
         if (d.data.datas.suppressErrors !== false) {
             if (d.data.datas.suppressErrors !== undefined) {
                 let output = d.data.datas.suppressErrors;
-                output = output.replaceAll('{fullError}', `\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1})**`)
-                output = output.replaceAll('{line}', `${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1}`)
-                output = output.replaceAll('{function}', d.data.name)
-                output = output.replaceAll('{errorMessage}', message)
+                output = output.replaceAll('{fullError}', `\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1})**`);
+                output = output.replaceAll('{line}', `${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1}`);
+                output = output.replaceAll('{function}', d.data.name);
+                output = output.replaceAll('{errorMessage}', message);
                 if (d.channel === undefined) {
-                    d.data.datas.isError = true
+                    d.data.datas.isError = true;
                 } else {
-                    d.data.datas.isError = true
-                    d.channel.send(output)
+                    d.data.datas.isError = true;
+                    d.channel.send(output);
                 }
-
             }
-            return
-        };
+            return;
+        }
         if (d.msg?.channel === undefined) {
-            d.data.datas.isError = true
-            console.error(`\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1})**`)
+            d.data.datas.isError = true;
+            console.error(`\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1})**`);
         } else {
-            d.data.datas.isError = true
-            d.msg.channel.send(`\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1})**`)
+            d.data.datas.isError = true;
+            d.msg.channel.send(`\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1})**`);
         }
     }
+
     const util = require("../handler/utils/Util.js");
-    let messagee = message?.msg || message
-    let d = data.name
+    let messagee = message?.msg || message;
+    let d = data.name;
     let all = {
         db: db,
         data: data,
@@ -46,9 +44,11 @@ module.exports = async (data, name, db, message, client, error, real) => {
         this: real,
         sendError: err,
         util
-    }
+    };
+
     let arg = all.msg?.content?.replace(all.client?.prefix, "").replace(all.cmd, "");
     arg = arg?.trim();
-    all.arg = arg
-    return await real.functions.get(d.toLowerCase())(all)
-}
+    all.arg = arg;
+    
+    return await real.functions.get(d.toLowerCase())(all);
+};
