7d8d6a68953f96d3f2aeccd80a04c4b5f6fbd5a8
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-05 18:46:40 +0700

    Update userTag.js

--- src/funcs/functions/info/userTag.js
+++ src/funcs/functions/info/userTag.js
@@ -4,8 +4,9 @@ module.exports = {
   description: "return user tag with provided id",
   code: async (d) => {
     let [id = d.author?.id] = d.data.splits;
-    id = id == d.author?.id ? d.author : d.client.users.cache.get(id);
-    if (!id) return d.sendError(d, "Invalid user id");
-    return id?.tag
+    let user = id === d.author?.id ? d.author : await d.client.users.fetch(id).catch(() => null);
+
+    if (!user) return d.sendError(d, "Invalid user id");
+    return user.tag;
   }
-}
+};
