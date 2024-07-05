1e2896cbb0779a5dc43fb992011a2fd6bf965df9
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-05 18:37:27 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,273 +1,225 @@
 const api = require("./handler/api.js");
-const getVersion = require("./handler/version.js");
 const version = require("../package.json").version;
-const Discord = require("discord.js");
+const { Client, GatewayIntentBits, Partials, ActivityType } = require("discord.js");
 const newMap = require("./cache Handler/cache.js");
 const Db = require("meatdb");
 const fs = require("fs");
 const path = require("path");
-const debug = require("debug")("ez:main");
-debug("Loaded");
+
 class Bot {
     constructor(opt) {
-        this.opt = opt
-        this.client = {}
-        this.prefix = opt.prefix
+        this.opt = opt;
+        this.client = {};
+        this.prefix = opt.prefix;
         this.db = new Db({
-            path: opt?.database?.path
-        })
-        this.cmd = require("./handler/commandType.js")
-        this.funcParser = require("./funcs/parser")
-        this.functions = new newMap()
-        this.variable = new newMap()
-        this.#start()
-        if (typeof this.prefix != "string") throw new Error("prefix must be string");
-        getVersion().then(z => {
-            const up = opt.autoUpdateVersion;
-            if (z != version && z !== undefined && up === true) {
-                console.log("Updating version... (autoUpdateVersion)")
-                require("child_process").execSync("npm i https://github.com/Bumblebee-3/easier-discord.js");
-                console.log("Update complete, please restart your bot");
-                process.exit();
-            }
-        })
+            path: opt?.database?.path,
+        });
+        this.cmd = require("./handler/commandType.js");
+        this.funcParser = require("./funcs/parser");
+        this.functions = new newMap();
+        this.variable = new newMap();
+        this.#start();
+        if (typeof this.prefix !== "string") throw new Error("prefix must be string");
     }
+
     //start client
     #start() {
-        debug('Bot#start')
-        const client = new Discord.Client(
-            this.opt
-        )
-        this.client = client
-        this.client.simpler = this
-        let dirFolder = path.join(__dirname,
-            "funcs",
-            "functions");
-
-        let folders = fs.readdirSync(dirFolder)
-        folders.forEach(x => {
-            let files = fs.readdirSync(path.join(dirFolder, x)).filter(file => file.endsWith('js'))
-            files.forEach(y => {
-                const file = require(`${path.join(dirFolder, x, y)}`)
-                this.functions.set("$" + y.replace(".js", "").toLowerCase(), file.code)
-            })
+        const client = new Client({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent,
+                GatewayIntentBits.GuildMembers,
+                GatewayIntentBits.GuildMessageReactions,
+            ],
+            partials: [
+                Partials.Message,
+                Partials.Channel,
+                Partials.Reaction,
+            ],
         });
+        this.client = client;
+        this.client.simpler = this;
+        let dirFolder = path.join(__dirname, "funcs", "functions");
+
+        let folders = fs.readdirSync(dirFolder);
+        folders.forEach(x => {
+            let files = fs.readdirSync(path.join(dirFolder, x)).filter(file => file.endsWith('js'));
+            files.forEach(y => {
+                const file = require(`${path.join(dirFolder, x, y)}`);
+                this.functions.set("$" + y.replace(".js", "").toLowerCase(), file.code);
+            });
+        });
+
         // Presence manager
         this.client.on('ready', async () => {
-            while(this.status.size > 0) {
-                for(let [k, v] of this.status) {
+            while (this.status.size > 0) {
+                for (let [k, v] of this.status) {
                     const session = this.client;
                     session.user.setPresence({
                         activities: [{
                             name: v.text,
-                            type: v.type.toUpperCase()
+                            type: ActivityType[v.type.toUpperCase()],
                         }],
-                        status: v.status
+                        status: v.status,
                     });
                     await sleep(ms(v.time));
                 }
             }
-        })
+        });
     }
-    //events
 
+    //events
     onBotJoin() {
-        this.client.on("guildCreate",
-            async (guild) => {
-                await require("./handler/command/botJoin.js")(guild, this)
-            })
+        this.client.on("guildCreate", async (guild) => {
+            await require("./handler/command/botJoin.js")(guild, this);
+        });
     }
 
     onBotLeave() {
-        this.client.on("guildDelete",
-            async (guild) => {
-                await require("./handler/command/botLeave.js")(guild, this)
-            })
+        this.client.on("guildDelete", async (guild) => {
+            await require("./handler/command/botLeave.js")(guild, this);
+        });
     }
 
     onMessage() {
-        debug('Bot#onMessage')
-        this.client.on("messageCreate",
-            async (msg) => {
-                debug('Bot#onMessage:func')
-                await require("./handler/command/default.js")(msg, this);
-                await require("./handler/command/always.js")(msg, this)
-            })
+        this.client.on("messageCreate", async (msg) => {
+            await require("./handler/command/default.js")(msg, this);
+            await require("./handler/command/always.js")(msg, this);
+        });
     }
 
     onMemberJoin() {
-        this.client.on("guildMemberAdd",
-            async (member) => {
-                await require("./handler/command/memberJoin.js")(member, this)
-            })
-    }
-    onMemberLeave() {
-        this.client.on("guildMemberRemove",
-            async (member) => {
-                await require("./handler/command/memberLeave.js")(member, this)
-            })
-    }
-    onReactionAdd() {
-        this.client.on("messageReactionAdd",
-            async (msg, emoji) => {
-                await require("./handler/command/reactionAdd.js")(msg, emoji, this)
-            })
+        this.client.on("guildMemberAdd", async (member) => {
+            await require("./handler/command/memberJoin.js")(member, this);
+        });
     }
 
+    onMemberLeave() {
+        this.client.on("guildMemberRemove", async (member) => {
+            await require("./handler/command/memberLeave.js")(member, this);
+        });
+    }
+
+    onReactionAdd() {
+        this.client.on("messageReactionAdd", async (reaction, user) => {
+            await require("./handler/command/reactionAdd.js")(reaction, user, this);
+        });
+    }
 
     //commands
-
     botJoinCommand(opt) {
-        this.cmd.botJoin.set(this.cmd.botJoin.size,
-            opt)
+        this.cmd.botJoin.set(this.cmd.botJoin.size, opt);
     }
 
     botLeaveCommand(opt) {
-        this.cmd.botLeave.set(this.cmd.botLeave.size,
-            opt)
+        this.cmd.botLeave.set(this.cmd.botLeave.size, opt);
     }
 
     command(...opts) {
-        debug('Bot#command',
-            opts)
         for (const opt of opts) {
             if (opt?.name !== "$always") {
-                this.cmd["default"].set(opt.name.toLowerCase(), opt)
+                this.cmd["default"].set(opt.name.toLowerCase(), opt);
             } else {
-                this.cmd["alwaysExecute"].set(this.cmd.alwaysExecute.size, opt)
+                this.cmd["alwaysExecute"].set(this.cmd.alwaysExecute.size, opt);
             }
         }
     }
 
     executableCommand(opt) {
-        this.cmd.executable.set(opt.name.toLowerCase(), opt)
+        this.cmd.executable.set(opt.name.toLowerCase(), opt);
     }
 
     intervalCommand(opt) {
         (async () => {
-            const commandData = opt.channel?.includes("$") ? await require("./handler/function.js")(
-                opt.channel,
-                "channel",
-                this.db,
-                {},
-                this.client,
-                this
-            ) : opt.channel || {};
+            const commandData = opt.channel?.includes("$") ? await require("./handler/function.js")(opt.channel, "channel", this.db, {}, this.client, this) : opt.channel || {};
             setInterval(async () => {
-                await require("./handler/function.js")(
-                    opt.code,
-                    "intervalCommand",
-                    this.db,
-                    commandData,
-                    this.client,
-                    this
-                )
-            }, opt.every)
+                await require("./handler/function.js")(opt.code, "intervalCommand", this.db, commandData, this.client, this);
+            }, opt.every);
             if (opt?.onStartup === true) {
                 this.client.on('ready', async () => {
-                    await require("./handler/function.js")(
-                        opt.code,
-                        "intervalCommand",
-                        this.db,
-                        commandData,
-                        this.client,
-                        this
-                    )
-                })
+                    await require("./handler/function.js")(opt.code, "intervalCommand", this.db, commandData, this.client, this);
+                });
             }
-        })()
+        })();
     }
 
     memberJoinCommand(opt) {
-        this.cmd.memberJoin.set(this.cmd.memberJoin.size,
-            opt)
+        this.cmd.memberJoin.set(this.cmd.memberJoin.size, opt);
     }
 
     memberLeaveCommand(opt) {
-        this.cmd.memberLeave.set(this.cmd.memberLeave.size,
-            opt)
+        this.cmd.memberLeave.set(this.cmd.memberLeave.size, opt);
     }
 
     reactionAddCommand(opt) {
-        this.cmd.reactionAdd.set(this.cmd.reactionAdd.size,
-            opt)
+        this.cmd.reactionAdd.set(this.cmd.reactionAdd.size, opt);
     }
 
     ready(opt) {
-        this.client.on("ready",
-            async () => {
-                await require("./handler/function.js")(opt.code, undefined, this.db, {}, this.client, this)
-            })
+        this.client.on("ready", async () => {
+            await require("./handler/function.js")(opt.code, undefined, this.db, {}, this.client, this);
+        });
     }
 
-
     variables(opt) {
-        debug('Bot#variables',
-            opt)
         for (const [name, value] of Object.entries(opt)) {
-            this.variable.set(name, value)
+            this.variable.set(name, value);
         }
     }
 
     async login(token) {
-        debug('Bot#login')
-        const current = await getVersion();
-        if (current != version && current !== undefined) console.log("\x1b[33m⚠Update Notice⚠\x1b[0m\nNew version available: " + current + "\nYour version: " + version);
-        await this.client.login(token)
+        await this.client.login(token);
         this.client.prefix = this.prefix;
-        console.log("Initialized on " + this.client.user.tag + "\nMade with : \x1b[32mSimple Discord\x1b[0m\nv" + version + "\nJoin official support server: https://discord.gg/DW4CCH236j");
-        api(this)
+        console.log("Initialized on " + this.client.user.tag + "\nMade with : Simple Discord\nv" + version + "\nJoin official support server: https://discord.gg/DW4CCH236j");
+        api(this);
     }
 
     //Custom Function
-
     createCustomFunction(opt) {
         if (!opt?.name || !opt?.name?.includes('$') || typeof opt?.code !== 'function') throw new Error('Invalid Name or Code');
-        this.functions.set(opt.name.toLowerCase(), opt.code)
+        this.functions.set(opt.name.toLowerCase(), opt.code);
     }
 
     // Add presence
     addPresence(...options) {
-        if(!options) throw new Error('Invalid presence options provided!');
+        if (!options) throw new Error('Invalid presence options provided!');
         options.map(s => {
-            if(ms(s.time) < 12000) throw new Error('Status time must be at least 12 seconds!');
+            if (ms(s.time) < 12000) throw new Error('Status time must be at least 12 seconds!');
             this.status.set(s.text, s);
         });
     }
 }
 
-
-
 class CommandHandler {
-    constructor(opts){
-        this.bot=opts.client||opts.bot;
+    constructor(opts) {
+        this.bot = opts.client || opts.bot;
     }
     load(folder) {
         let bot = this.bot;
         let consoleText = [];
         let dirFolder = path.join(process.cwd(), folder);
 
-        let files = fs.readdirSync(dirFolder).filter(file => file.endsWith('js'))
+        let files = fs.readdirSync(dirFolder).filter(file => file.endsWith('js'));
         files.forEach(x => {
             try {
-                const theFile = require(`${dirFolder}/${x}`)
-                const theCmd = bot.cmd[theFile?.type || "default"]
+                const theFile = require(`${dirFolder}/${x}`);
+                const theCmd = bot.cmd[theFile?.type || "default"];
                 if (theCmd !== undefined) {
-                    theCmd.set(theFile.name, theFile)
-                    consoleText.push('Loaded ' + dirFolder + '/' + x)
+                    theCmd.set(theFile.name, theFile);
+                    consoleText.push('Loaded ' + dirFolder + '/' + x);
+                } else {
+                    consoleText.push("Command type is invalid " + dirFolder + "/" + x);
                 }
-                else consoleText.push("Command type is invalid " + dirFolder + "/" + x);
-            }
-            catch (e) {
-                consoleText.push('Faled to load ' + dirFolder + '/' + x)
+            } catch (e) {
+                consoleText.push('Failed to load ' + dirFolder + '/' + x);
             }
         });
-        console.log(consoleText.join('\n|-------------------------------|\n'))
+        console.log(consoleText.join('\n|-------------------------------|\n'));
     }
 }
-
 
 module.exports = {
     Bot,
     CommandHandler
-}
+};
