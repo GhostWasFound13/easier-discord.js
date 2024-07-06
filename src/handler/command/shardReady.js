de4b08e447bfdf5da930a2f897aadf43134d0ee6
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-06 18:49:52 +0700

    Update shardReady.js

--- src/handler/command/shardReady.js
+++ src/handler/command/shardReady.js
@@ -1,14 +1,21 @@
-module.exports = async (id, guilds, bot, cmd) => {
+module.exports = async (id, guilds, bot) => {
+  if (!bot || !bot.cmd || !bot.cmd.shardReady) {
+    console.error("bot.cmd.shardReady is not defined");
+    return;
+  }
+
   let data = { shardID: id, guilds };
   const cmds = bot.cmd.shardReady.values();
+
   for (const cmd of cmds) {
     if (cmd?.channel?.includes("$")) {
       data.channel = bot.client.channels.cache.get(
         await require("../function.js")(cmd.channel, "channeleval", bot.db, data, bot.client, bot)
       );
     } else {
-      data.channel = bot.client.channels.cache.get(id);
+      data.channel = bot.client.channels.cache.get(cmd?.channel);
     }
+
     require("../function.js")(cmd.code, "shardReady", bot.db, data, bot.client, bot);
   }
 };
