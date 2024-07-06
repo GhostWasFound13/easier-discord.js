c4f482c0004e16899a5626becf02d051b66f25fe
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-06 18:30:32 +0700

    Update shardReconnecting.js

--- src/handler/command/shardReconnecting.js
+++ src/handler/command/shardReconnecting.js
@@ -1,7 +1,14 @@
 module.exports = async (id, bot) => {
-    let data = { shardID: id };
-    const cmds = bot.cmd.shardReconnecting.array();
-    for (const cmd of cmds) {
-        await require("../function.js")(cmd.code, "shardReconnecting", bot.db, data, bot.client, bot);
+  let data = { shardID: id };
+  const cmds = bot.cmd.shardReconnecting.values();
+  for (const cmd of cmds) {
+    if (cmd?.channel?.includes("$")) {
+      data.channel = bot.client.channels.cache.get(
+        await require("../function.js")(cmd.channel, "channeleval", bot.db, data, bot.client, bot)
+      );
+    } else {
+      data.channel = bot.client.channels.cache.get(cmd.channel);
     }
-}
+    require("../function.js")(cmd.code, "shardReconnecting", bot.db, data, bot.client, bot);
+  }
+};
