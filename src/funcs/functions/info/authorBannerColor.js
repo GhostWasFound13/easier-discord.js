05aad4304436ce1c3e6ca0e7b85d7fd5e56d6f01
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-06 18:00:10 +0700

    Update authorBannerColor.js
    This will work omg

--- src/funcs/functions/info/authorBannerColor.js
+++ src/funcs/functions/info/authorBannerColor.js
@@ -1,10 +1,12 @@
 module.exports = {
   name: "$authorBannerColor",
   usage: "",
-  description: "display author banner color return color of author banner",
+  description: "display author banner color\nreturn color of author banner",
   code: async (d) => {
     const user = await d.client.users.fetch(d.author.id, { force: true });
-    const bannerColor = user.bannerColor ? user.bannerColor.toString() return d.sendError(d, "No banner color");
-    return bannerColor;
+    if (!user.bannerColor) {
+      return d.sendError(d, "No banner color");
+    }
+    return user.bannerColor.toString();
   }
 };
