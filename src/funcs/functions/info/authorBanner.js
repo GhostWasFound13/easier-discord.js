79b7f8d78798508e865c1abb454c66949ab08f4c
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-06 17:39:28 +0700

    Update authorBanner.js

--- src/funcs/functions/info/authorBanner.js
+++ src/funcs/functions/info/authorBanner.js
@@ -3,7 +3,7 @@ module.exports = {
   usage: "[size(optional);dynamic(optional);format(optional)]",
   description: "display author banner\nreturn link of author banner",
   code: async (d) => {
-    const [size = 4096, dynamic = "yes", format = "webp"] = d.data.splits;
+    let [size = 4096, dynamic = "yes", format = "webp"] = d.data.splits;
 
     // Ensure the format is one of the valid formats supported by Discord.js v14
     const validFormats = ['webp', 'png', 'jpg', 'jpeg'];
@@ -11,13 +11,11 @@ module.exports = {
       format = 'webp';
     }
 
-    try {
-      const user = await d.client.users.fetch(d.author.id, { force: true });
-      return user.bannerURL({
-        size: Number(size),
-        dynamic: dynamic === 'yes',
-        extension: format
-      })
-    } 
+    const user = await d.client.users.fetch(d.author.id, { force: true });
+    return user.bannerURL({
+      size: Number(size),
+      dynamic: dynamic === 'yes',
+      format: format
+    });
   }
 };
