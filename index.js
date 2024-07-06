d82ccfc99a0511d475422355823fe687628b3079
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2024-07-06 07:01:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bot = new Bot({
         "GuildMessageReactions"
     ],//create your bot intents, put this with your needed
     prefix: "!", //set your bot command prefix to !
-   partials: ["Message", "Channel", "Reaction"]
+   partials: ["Message", "Channel", "Reaction"] // partial needed
 });
 bot.onMessage() //callback that execute command when there's message send, put this once in your bot
 bot.command({
