ac0c157a0cc5a672900feb236f9d147a5d60e6af
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2025-01-07 15:07:15 +0700

    Update bot.ts

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,10 +1,18 @@
-import { Client, GatewayIntentBits, Partials, ActivityType, ClientOptions, PresenceData } from "discord.js";
+import {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  ActivityType,
+  ClientOptions,
+  PresenceData,
+} from "discord.js";
 import { readdirSync } from "fs";
 import { join } from "path";
 import Db from "meatdb";
 import api from "./handler/api";
 import { version } from "../package.json";
 import newMap from "./cache Handler/cache";
+import { CommandHandler } from "./CommandHandler"; // Import CommandHandler here
 
 type CommandOptions = Record<string, unknown>;
 type PresenceOptions = {
@@ -61,7 +69,6 @@ class Bot {
     this.#start();
   }
 
-  // Start client
   #start() {
     const clientOptions: ClientOptions = {
       intents: this.opt.intents.map((intent) => GatewayIntentBits[intent]),
@@ -71,7 +78,6 @@ class Bot {
     this.client = new Client(clientOptions);
     this.client.simpler = this;
 
-    // Load functions
     const dirFolder = join(__dirname, "funcs", "functions");
     const folders = readdirSync(dirFolder);
     folders.forEach((folder) => {
@@ -84,7 +90,6 @@ class Bot {
       });
     });
 
-    // Presence manager
     this.client.on("ready", async () => {
       while (this.status.size > 0) {
         for (const [, v] of this.status) {
@@ -102,51 +107,11 @@ class Bot {
       }
     });
 
-    // Shard events
     this.onShardReady();
     this.onShardDisconnect();
     this.onShardReconnecting();
   }
 
-  // Events
-  onBotJoin() {
-    this.client.on("guildCreate", async (guild) => {
-      await require("./handler/command/botJoin")(guild, this);
-    });
-  }
-
-  onBotLeave() {
-    this.client.on("guildDelete", async (guild) => {
-      await require("./handler/command/botLeave")(guild, this);
-    });
-  }
-
-  onMessage() {
-    this.client.on("messageCreate", async (msg) => {
-      await require("./handler/command/default")(msg, this);
-      await require("./handler/command/always")(msg, this);
-    });
-  }
-
-  onMemberJoin() {
-    this.client.on("guildMemberAdd", async (member) => {
-      await require("./handler/command/memberJoin")(member, this);
-    });
-  }
-
-  onMemberLeave() {
-    this.client.on("guildMemberRemove", async (member) => {
-      await require("./handler/command/memberLeave")(member, this);
-    });
-  }
-
-  onReactionAdd() {
-    this.client.on("messageReactionAdd", async (reaction, user) => {
-      await require("./handler/command/reactionAdd")(reaction, user, this);
-    });
-  }
-
-  // Shard events
   onShardReady() {
     this.client.on("shardReady", async (id) => {
       await require("./handler/command/shardReady")(id, this);
@@ -165,64 +130,6 @@ class Bot {
     });
   }
 
-  // Commands
-  botJoinCommand(opt: CommandOptions) {
-    this.cmd.botJoin.set(this.cmd.botJoin.size, opt);
-  }
-
-  botLeaveCommand(opt: CommandOptions) {
-    this.cmd.botLeave.set(this.cmd.botLeave.size, opt);
-  }
-
-  command(...opts: CommandOptions[]) {
-    for (const opt of opts) {
-      if (opt.name !== "$always") {
-        this.cmd.default.set(opt.name.toLowerCase(), opt);
-      } else {
-        this.cmd.alwaysExecute.set(this.cmd.alwaysExecute.size, opt);
-      }
-    }
-  }
-
-  executableCommand(opt: CommandOptions) {
-    this.cmd.executable.set(opt.name.toLowerCase(), opt);
-  }
-
-  intervalCommand(opt: CommandOptions & { every: number; onStartup?: boolean }) {
-    const executeInterval = async () => {
-      const commandData = opt.channel?.includes("$")
-        ? await require("./handler/function")(opt.channel, "channel", this.db, {}, this.client, this)
-        : opt.channel || {};
-      setInterval(async () => {
-        await require("./handler/function")(opt.code, "intervalCommand", this.db, commandData, this.client, this);
-      }, opt.every);
-      if (opt?.onStartup) {
-        this.client.on("ready", async () => {
-          await require("./handler/function")(opt.code, "intervalCommand", this.db, commandData, this.client, this);
-        });
-      }
-    };
-    executeInterval();
-  }
-
-  memberJoinCommand(opt: CommandOptions) {
-    this.cmd.memberJoin.set(this.cmd.memberJoin.size, opt);
-  }
-
-  memberLeaveCommand(opt: CommandOptions) {
-    this.cmd.memberLeave.set(this.cmd.memberLeave.size, opt);
-  }
-
-  reactionAddCommand(opt: CommandOptions) {
-    this.cmd.reactionAdd.set(this.cmd.reactionAdd.size, opt);
-  }
-
-  ready(opt: CommandOptions) {
-    this.client.on("ready", async () => {
-      await require("./handler/function")(opt.code, undefined, this.db, {}, this.client, this);
-    });
-  }
-
   variables(opt: Record<string, unknown>) {
     for (const [name, value] of Object.entries(opt)) {
       this.variable.set(name, value);
@@ -238,15 +145,6 @@ class Bot {
     api(this);
   }
 
-  // Custom Function
-  createCustomFunction(opt: { name: string; code: Function }) {
-    if (!opt?.name?.includes("$") || typeof opt?.code !== "function") {
-      throw new Error("Invalid Name or Code");
-    }
-    this.functions.set(opt.name.toLowerCase(), opt.code);
-  }
-
-  // Add presence
   addPresence(...options: PresenceOptions[]) {
     if (!options) throw new Error("Invalid presence options provided!");
     options.forEach((s) => {
@@ -256,35 +154,4 @@ class Bot {
   }
 }
 
-class CommandHandler {
-  private bot: Bot;
-
-  constructor(opts: { client?: Bot; bot?: Bot }) {
-    this.bot = opts.client || opts.bot!;
-  }
-
-  load(folder: string) {
-    const dirFolder = join(process.cwd(), folder);
-    const consoleText: string[] = [];
-
-    const files = readdirSync(dirFolder).filter((file) => file.endsWith(".js"));
-    files.forEach((x) => {
-      try {
-        const theFile = require(`${dirFolder}/${x}`);
-        const theCmd = this.bot.cmd[theFile?.type || "default"];
-        if (theCmd) {
-          theCmd.set(theFile.name, theFile);
-          consoleText.push("Loaded " + dirFolder + "/" + x);
-        } else {
-          consoleText.push("Command type is invalid " + dirFolder + "/" + x);
-        }
-      } catch (e) {
-        consoleText.push("Failed to load " + dirFolder + "/" + x);
-      }
-    });
-
-    console.log(consoleText.join("\n|-------------------------------|\n"));
-  }
-}
-
-export { Bot, CommandHandler };
+export { Bot };
